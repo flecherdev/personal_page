aff0ae20d22e53c38b28dcab8681a824cf75b768
Ezequiel Freire <ezequiel.a.freire@gmail.com>
2020-08-18 19:23:24 -0300

    se agrega mod y se lista los projectos

--- src/page/Projects.js
+++ src/page/Projects.js
@@ -2,26 +2,29 @@ import React from 'react'
 import Nav from '../components/Nav';
 import '../components/style/Projects.css'
 
+// https://fir-crud-b03d8.web.app/cars
+// https://comandautn.web.app/
+
 class Projects extends React.Component {
     state = {
         data: [
             {
                 id: "1",
                 title: "Comanda UTN",
-                description: "Esta web app esta diseñada para generar un pedido y que este vaya pasando por diferentes empleados hasta el momento de cobrar, similar a las cadenas de comida rapida.",
-                image: ""
+                description: "Esta web app esta diseñada para generar un pedido y que este vaya pasando por diferentes empleados hasta el momento de cobrar, similar a las cadenas de comida rapida. Las tecnologias utilizadas son Angular 9 (FrontEnd) y SlimFramework(BackEnd)",
+                image: "https://firebasestorage.googleapis.com/v0/b/ezequielfreiredev.appspot.com/o/comandautn.png?alt=media&token=d9402a5b-3b4b-4108-a16a-04707159cdba"
             },
             {
                 id: "2",
                 title: "Car Crash",
-                description: "Es una web app que filtra autos segun caterioas",
-                image: ""
+                description: "Es una web app que filtra autos segun caterioas. Se utilizo Angular 9 y Angular Material",
+                image: "https://firebasestorage.googleapis.com/v0/b/ezequielfreiredev.appspot.com/o/carscrash.png?alt=media&token=e283bbea-8af3-44a2-bd04-f4f38371b3c7"
             },
             {
                 id: "3",
-                title: "",
-                description: "",
-                image: ""
+                title: "Tienda de compras",
+                description: "Tienda de compras es un proyecto realizado en el curso de Angular en Platzi.",
+                image: "https://firebasestorage.googleapis.com/v0/b/ezequielfreiredev.appspot.com/o/platzi-store.png?alt=media&token=3af875e5-b56b-4391-ae06-72c2cf31213f"
             }
         ]
     }
@@ -30,30 +33,21 @@ class Projects extends React.Component {
             <div>
                 <Nav />
                 <div className="Projects">
-                    <div className="Projects__Card card" >
-                        <img src="..." className="card-img-top" alt="..."/>
-                        <div class="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
-                    <div className="Projects__Card card" >
-                        <img src="..." className="card-img-top" alt="..."/>
-                        <div class="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
-                    <div className="Projects__Card card" >
-                        <img src="..." className="card-img-top" alt="..."/>
-                        <div class="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
+                    {this.state.data.map((project) => {
+                        return(
+                            <div className="Projects__Card card" >
+                                <img src={project.image} className="card-img-top" alt=""/>
+                                <div class="card-body">
+                                    <h5 className="card-title">{project.title}</h5>
+                                    <p className="card-text">{project.description}</p>
+                                    <div class="card-footer">
+                                        <a href="#" className="btn btn-primary">Code github</a>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    })}
+                    
                 </div>
             </div>
         )
