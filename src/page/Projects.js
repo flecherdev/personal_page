ef74fc3754d02afc1267fbf0a31bf45fbbe53d9d
Ezequiel Freire <ezequiel.a.freire@gmail.com>
2020-08-19 23:17:16 -0300

    se agrega link a projectos

--- src/page/Projects.js
+++ src/page/Projects.js
@@ -1,10 +1,7 @@
 import React from 'react'
-import Nav from '../components/Nav';
+
 import ProjectList from '../components/ProjectList';
 import '../components/style/Projects.css'
-
-// https://fir-crud-b03d8.web.app/cars
-// https://comandautn.web.app/
 
 class Projects extends React.Component {
     state = {
@@ -13,28 +10,34 @@ class Projects extends React.Component {
                 id: "1",
                 title: "Comanda UTN",
                 description: "Esta web app esta diseñada para generar un pedido y que este vaya pasando por diferentes empleados hasta el momento de cobrar, similar a las cadenas de comida rapida. Las tecnologias utilizadas son Angular 9 (FrontEnd) y SlimFramework(BackEnd)",
-                image: "https://firebasestorage.googleapis.com/v0/b/ezequielfreiredev.appspot.com/o/comandautn.png?alt=media&token=d9402a5b-3b4b-4108-a16a-04707159cdba"
+                image: "https://firebasestorage.googleapis.com/v0/b/ezequielfreiredev.appspot.com/o/comandautn.png?alt=media&token=d9402a5b-3b4b-4108-a16a-04707159cdba",
+                github: "https://github.com/flecherdev/comanda-front",
+                link: "https://comandautn.web.app/"
+                
             },
             {
                 id: "2",
                 title: "Car Crash",
                 description: "Es una web app que filtra autos segun caterioas. Se utilizo Angular 9 y Angular Material",
-                image: "https://firebasestorage.googleapis.com/v0/b/ezequielfreiredev.appspot.com/o/carscrash.png?alt=media&token=e283bbea-8af3-44a2-bd04-f4f38371b3c7"
+                image: "https://firebasestorage.googleapis.com/v0/b/ezequielfreiredev.appspot.com/o/carscrash.png?alt=media&token=e283bbea-8af3-44a2-bd04-f4f38371b3c7",
+                github: "https://github.com/flecherdev/filter-car",
+                link: "https://comandautn.web.app/"
             },
             {
                 id: "3",
                 title: "Tienda de compras",
                 description: "Tienda de compras es un proyecto realizado en el curso de Angular en Platzi.",
-                image: "https://firebasestorage.googleapis.com/v0/b/ezequielfreiredev.appspot.com/o/platzi-store.png?alt=media&token=3af875e5-b56b-4391-ae06-72c2cf31213f"
+                image: "https://firebasestorage.googleapis.com/v0/b/ezequielfreiredev.appspot.com/o/platzi-store.png?alt=media&token=3af875e5-b56b-4391-ae06-72c2cf31213f",
+                github: "https://github.com/flecherdev/platzi-store",
+                link: ""
             }
         ]
     }
     render() {
         return (
-            <div>
-                <Nav />
+            <React.Fragment>
                 <ProjectList project={this.state.data}/>
-            </div>
+            </React.Fragment>
         )
     }
 }
